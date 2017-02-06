2bb67d513b8b48322dc80c0f356bdae81a47454a
Supratim Chowdhury <supratims@gmail.com>
2017-02-06 17:48:43 +0000

    More improvements, to add new dimension

--- reports/EventsByCountry.php
+++ reports/EventsByCountry.php
@@ -10,7 +10,7 @@ class EventsByCountry extends AbstractReports {
 
 		// Create the DateRange object.
 		$dateRange = new Google_Service_AnalyticsReporting_DateRange();
-		$dateRange->setStartDate("7daysAgo");
+		$dateRange->setStartDate("1daysAgo");
 		$dateRange->setEndDate("today");
 
 		// Create the Metrics object.
@@ -26,27 +26,36 @@ class EventsByCountry extends AbstractReports {
 		$dimension_action = new Google_Service_AnalyticsReporting_Dimension();
 		$dimension_action->setName("ga:eventAction");
 
+		// Add another dimension by city
+		$dimension_city = new Google_Service_AnalyticsReporting_Dimension();
+		$dimension_city->setName("ga:city");
+
 		// Add another dimension by country
 		$dimension_country = new Google_Service_AnalyticsReporting_Dimension();
-		$dimension_country->setName("ga:city");
+		$dimension_country->setName("ga:country");
 
 		//apply a Dimension filter clause and filter only the eventAction with value 'call'
+		// Create Dimension Filter for matching dimension 'call'
+		$dimension_filter_call = new Google_Service_AnalyticsReporting_DimensionFilter();
+		$dimension_filter_call->setDimensionName("ga:eventAction");
+		$dimension_filter_call->setOperator("EXACT");
+		$dimension_filter_call->setExpressions(["call"]);
 
-		// Create Dimension Filter.
-		$dimension_filter = new Google_Service_AnalyticsReporting_DimensionFilter();
-		$dimension_filter->setDimensionName("ga:eventAction");
-		$dimension_filter->setOperator("EXACT");
-		$dimension_filter->setExpressions(["call"]);
+		// Create Dimension Filter for matching dimension 'enquiry'
+		$dimension_filter_enquiry = new Google_Service_AnalyticsReporting_DimensionFilter();
+		$dimension_filter_enquiry->setDimensionName("ga:eventAction");
+		$dimension_filter_enquiry->setOperator("EXACT");
+		$dimension_filter_enquiry->setExpressions(["enquiry"]);
 
 		$dimension_filter_clause = new Google_Service_AnalyticsReporting_DimensionFilterClause();
-		$dimension_filter_clause->setFilters($dimension_filter);
+		$dimension_filter_clause->setFilters([$dimension_filter_call, $dimension_filter_enquiry]);
 
 		// Create the ReportRequest object.
 		$request = new Google_Service_AnalyticsReporting_ReportRequest();
 		$request->setViewId($VIEW_ID);
 		$request->setDateRanges($dateRange);
 		$request->setMetrics([$metric]);
-		$request->setDimensions([$dimension, $dimension_country]);
+		$request->setDimensions([$dimension, $dimension_city, $dimension_country]);
 		$request->setDimensionFilterClauses([$dimension_filter_clause]);
 
 		$body = new Google_Service_AnalyticsReporting_GetReportsRequest();
